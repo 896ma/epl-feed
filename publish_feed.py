3dc0dee91790ce27262547832a5f71edbf6d9d84
G_oaT <100561547+896ma@users.noreply.github.com>
2023-09-27 21:45:10 +0300

    Update publish_feed.py

--- publish_feed.py
+++ publish_feed.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 from atproto.xrpc_client.models import ids
 
-from atproto import Client, mod
+from atproto import Client, m
 
 # YOUR bluesky handle
 # Ex: user.bsky.social
