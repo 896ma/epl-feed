17c6ff8f948eee97987c4c5741d8d7a851d7e2de
G_oaT <100561547+896ma@users.noreply.github.com>
2023-10-09 00:44:44 +0300

    Update config.py

--- server/config.py
+++ server/config.py
@@ -10,7 +10,7 @@ if HOSTNAME is None:
     raise RuntimeError('You should set "HOSTNAME" environment variable first.')
 
 if SERVICE_DID is None:
-    SERVICE_DID = f'did:web:{H}'
+    SERVICE_DID = f'did:web:{HOs}'
 
 
 EPL_FEED_URI = os.environ.get('EPL_FEED_URI')
