2508eb6691c32287ebc5075698ee71a0cb7fa4ce
G_oaT <100561547+896ma@users.noreply.github.com>
2023-10-08 12:28:51 +0300

    Update publish_feed.py

--- publish_feed.py
+++ publish_feed.py
@@ -6,7 +6,7 @@ load_dotenv()
 # YOU MUST INSTALL ATPROTO SDK
 # pip3 install atproto
 
-from datetime import datetime
+from datetime import dateti
 from atproto.xrpc_client.models import ids
 
 from atproto import  client , models
