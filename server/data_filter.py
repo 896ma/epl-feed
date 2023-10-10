2f2c9c1ff62da6669d379544b76d8f824e8e489d
G_oaT <100561547+896ma@users.noreply.github.com>
2023-10-10 19:57:43 +0300

    Update data_filter.py

--- server/data_filter.py
+++ server/data_filter.py
@@ -22,7 +22,7 @@ epl_key_names = [
     'newcastle united',
     'norwich city',
     'southampton',
-    'tottenham h
+    'tottenham ho
     'watford ' ,
     'west ham united',
     'wolverhampton wanderers',
