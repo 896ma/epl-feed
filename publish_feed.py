6f93c15fa31009ec6c3793961b5e6c7ab9d65093
G_oaT <100561547+896ma@users.noreply.github.com>
2023-10-25 00:37:31 +0300

    Update publish_feed.py

--- publish_feed.py
+++ publish_feed.py
@@ -23,7 +23,7 @@ PASSWORD: str = os.environ.get('PASSWORD', None)
 # Ex: feed.bsky.dev
 HOSTNAME: str = os.environ.get('EPL_HOSTNAME', None)
 
-# A short name for the record that will show in urls(Uniform Resource Allocator
+# A short name for the record that will show in urls(Uniform Resource Allocator)
 # Lowercase with no spaces.
 # Ex: whats-hot
 RECORD_NAME: str = 'epl-buzz'   
